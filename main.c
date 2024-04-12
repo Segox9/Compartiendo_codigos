52a5e642981117e0038b134a9368b15b36be0402
NDMDevel <damian.n.marasco@gmail.com>
2024-04-11 21:52:45 -0300

    Using ftell() and fseek()

--- main.c
+++ main.c
@@ -2,30 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 20
-
 int main()
 {
-    FILE *f    = fopen("file.dat","wb");
-    FILE *ftxt = fopen("file.txt","wb");
-    if( f == NULL || ftxt == NULL )
-    {
-        printf("error opening the file");
-        return 0;
-    }
-    struct Data d[N];
-    for( int i=0 ; i<N ; i++ )
-    {
-        d[i].p.x = rand() % 100;
-        d[i].p.y = rand() % 100;
-        d[i].temp = rand() % 100;
-        fwrite(d+i,sizeof(struct Data),1,f);
-        fprintf(ftxt,"x: %f\n",d[i].p.x);
-        fprintf(ftxt,"y: %f\n",d[i].p.y);
-        fprintf(ftxt,"temp: %f\n",d[i].temp);
-        fprintf(ftxt,"---------------------------\n");
-    }
+    FILE *f    = fopen("file.dat","rb");
+
+    printf("ftell(): %d\n", ftell(f) );
+
+//    struct Data d[1000];
+//    fread( d , sizeof(struct Data) , ? , f );
+
+    char c;
+    fread( &c , sizeof(char) , 1 , f );
+    fread( &c , sizeof(char) , 1 , f );
+    fread( &c , sizeof(char) , 1 , f );
+    fread( &c , sizeof(char) , 1 , f );
+    fread( &c , sizeof(char) , 1 , f );
+
+    printf("ftell(): %d\n", ftell(f) );
+
+    fseek( f , -2 , SEEK_CUR );
+    printf("ftell(): %d\n", ftell(f) );
+
     fclose(f);
-    fclose(ftxt);
     return 0;
 }
